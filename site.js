01e602000f705be9306228023c88d8a129ac99f1
davonab <davon.branch42@yahoo.com>
2018-04-03 22:34:27 -0500

    try to mess around and fix javascript

--- site.js
+++ site.js
@@ -1,13 +1,20 @@
 $.noConflict();
 (function($) {
-  $('#uinformation').on('submit', function(d) {
-    d.preventDefault();
+  $('#uinformation').on('submit', function(d)
+ {
+   d.preventDefault();
     // check if first name input box is empty
-    if(document.getElementById("fname").value === '' ){
-      $('#fname').after('<li id="error">Your first name is missing!</li>');
+    if(document.getElementById("fname").value !== '' && document.getElementById("lname").value !== '' && document.getElementById("number").value !== '' && document.getElementById("email").value !== '')
+     {
+       // remove the error messages
+       $('#error').remove();
+          console.log('form sub');
+     }
+    if(document.getElementById("fname").value === '' || document.getElementById("lname").value === '' || document.getElementById("number").value === '' || document.getElementById("email") === ''){
+      $('#header2').after('<li id="error">You have information missing!</li>');
     }
     // check if last name input box is empty
-    if(document.getElementById("lname").value === '' ){
+    /*if(document.getElementById("lname").value === '' ){
       $('#lname').after('<li id="error">Your last name is missing!</li>');
     }
     //check if phone number input box is empty
@@ -17,13 +24,6 @@ $.noConflict();
     //check if email input box is empty
     if(document.getElementById("email").value === '' ){
       $('#email').after('<li id="error">Your email is missing!</li>');
-    }
-    //check if there is data in the input boxes
-    if(document.getElementById("fname").value !== '' && document.getElementById("lname").value !== ''
-    && document.getElementById("number").value !== '' && document.getElementById("email").value !== '')
-    {
-      // remove the error messages
-       $('#error').remove(); //document.getElementById("error").remove();
-    }
+    }*/
   });
 })(jQuery);
